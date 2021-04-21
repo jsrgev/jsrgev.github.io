92db4f67484a0a939c6d58f1b3a2cb89af10ace2
Josh Regev <jsrgev@gmail.com>
2021-04-21 11:25:19 +0300

    improved madlibs

--- madlibs.js
+++ madlibs.js
@@ -5,12 +5,12 @@ button.addEventListener("click",generate);
 let previousRandom = "";
 
 function generate() {
-	for (input of inputs) {
-		if (input.value == "") { 
-			alert("Please fill in all the words!");
-			return;
-		}
-	}
+	// for (input of inputs) {
+	// 	if (input.value == "") { 
+	// 		alert("Please fill in all the words!");
+	// 		return;
+	// 	}
+	// }
 	let span = document.querySelector("#story");
 	span.textContent="";
 
@@ -23,11 +23,11 @@ function generate() {
 
 
 let stories =
-	[`I was waiting at a small train station when a ${adjective1} man put up a sign regarding my train: “30-Minute Delay.” “What happened?” I asked. “The conductor was busy ${verb} and the train ran into a ${adjective2} ${noun},” he said. “How long will that take to fix?” “Quite a few hours. ${name} has to get here from ${place}, and then there's still the ${noun} to fix too.” “So why put up a sign saying it would take 30 minutes?” “It’s the only sign we have.”`,
-	`I was waiting outside the post office one day when ${name} came over to me and asked, "Do you know who that ${adjective1} ${noun} across the street belongs to? I'd like to take it with me back to my ${adjective2} house in ${place}." "Go back to your ${verb}," I answered, "that ${noun} is staying right where it is.`,
-	`Last year my friends and I went to ${place} to celebrate my birthday. One friend gave me a ${adjective2} ${noun}. Another gave me a ${adjective1} ${noun}. Since I had two, I gave the ${adjective1} one to ${name}, who couldn't stop ${verb} from excitement.`,
-	`${name} awoke from a terrible nightmare. In the dream, there was a ${noun}, and then another, and another - each more ${adjective2} than the last, all ${verb} in a row. "Now you're coming with us to ${place}!" they shouted. "But how could I go there?!" ${name} cried. "It's too ${adjective1}!`,
-	`I wasn't feeling well, so I went to the doctor to get checked out. After the examination, she said "I'm afraid it's your ${noun}. It's far too ${adjective2}. But this condition isn't uncommon in people who come back from ${place}. ${name} had the same thing just last week. I recommend a lot of ${verb} until you feel more ${adjective1}.`]
+	[`<p>I was waiting at a small train station when a ${adjective1} man put up a sign regarding my train: “30-Minute Delay.” “What happened?” I asked.</p><p>“The conductor was busy ${verb} and the train ran into a ${adjective2} ${noun},” he said.</p><p>“How long will that take to fix?”</p><p>“Quite a few hours," he replied. " ${name} has to get here from ${place}, and then there's still the ${noun} to fix too.”</p><p>“So why put up a sign saying it would take 30 minutes?”</p><p>“It’s the only sign we have.”</p>`,
+	`<p>I was waiting outside the post office one day when ${name} came over to me and asked, "Do you know who that ${adjective1} ${noun} across the street belongs to? I'd like to take it with me back to my ${adjective2} house in ${place}."</p><p>"Go back to your ${verb}," I answered, "that ${noun} is staying right where it is.`,
+	`<p>Last year my friends and I went to ${place} to celebrate my birthday. One friend gave me a ${adjective2} ${noun}. Another gave me a ${adjective1} ${noun}. Since I had two, I gave the ${adjective1} one to ${name}, who couldn't stop ${verb} from excitement.</p>`,
+	`<p>${name} awoke from a terrible nightmare. In the dream, there was a ${noun}, and then another, and another - each more ${adjective2} than the last, all ${verb} in a row. "Now you're coming with us to ${place}!" they shouted.</p><p>"But how could I go there?!" ${name} cried. "It's too ${adjective1}!</p>`,
+	`<p>I wasn't feeling well, so I went to the doctor to get checked out. After the examination, she said "I'm afraid it's your ${noun}. It's far too ${adjective2}. But this condition isn't uncommon in people who come back from ${place}. ${name} had the same thing just last week. I recommend a lot of ${verb} until you feel more ${adjective1}."</p>`]
 
 	let randomNum;
 	do {
@@ -36,8 +36,8 @@ let stories =
 	while (randomNum == previousRandom);
 	previousRandom = randomNum;
 
-	let story = document.createTextNode(stories[randomNum])
-	span.appendChild(story);
+	let story = stories[randomNum];
+	span.innerHTML = story;
 }
 
 
