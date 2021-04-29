c1b7e916c20a52bf9f32a3e8ea2c18b02785ab89
Josh Regev <jsrgev@gmail.com>
2021-04-30 00:43:29 +0300

    refined filtering to narrow down if there are multiple segments in input

--- robofriends/script.js
+++ robofriends/script.js
@@ -1,119 +1,135 @@
-const robots = [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      username: 'Bret',
-      email: 'Sincere@april.biz',
-      image: 'https://robohash.org/1?200x200'
-    },
-    {
-      id: 2,
-      name: 'Ervin Howell',
-      username: 'Antonette',
-      email: 'Shanna@melissa.tv',
-      image: 'https://robohash.org/2?200x200'
-    },
-    {
-      id: 3,
-      name: 'Clementine Bauch',
-      username: 'Samantha',
-      email: 'Nathan@yesenia.net',
-      image: 'https://robohash.org/3?200x200'
-    },
-    {
-      id: 4,
-      name: 'Patricia Lebsack',
-      username: 'Karianne',
-      email: 'Julianne.OConner@kory.org',
-      image: 'https://robohash.org/4?200x200'
-    },
-    {
-      id: 5,
-      name: 'Chelsey Dietrich',
-      username: 'Kamren',
-      email: 'Lucio_Hettinger@annie.ca',
-      image: 'https://robohash.org/5?200x200'
-    },
-    {
-      id: 6,
-      name: 'Mrs. Dennis Schulist',
-      username: 'Leopoldo_Corkery',
-      email: 'Karley_Dach@jasper.info',
-      image: 'https://robohash.org/6?200x200'
-    },
-    {
-      id: 7,
-      name: 'Kurtis Weissnat',
-      username: 'Elwyn.Skiles',
-      email: 'Telly.Hoeger@billy.biz',
-      image: 'https://robohash.org/7?200x200'
-    },
-    {
-      id: 8,
-      name: 'Nicholas Runolfsdottir V',
-      username: 'Maxime_Nienow',
-      email: 'Sherwood@rosamond.me',
-      image: 'https://robohash.org/8?200x200'
-    },
-    {
-      id: 9,
-      name: 'Glenna Reichert',
-      username: 'Delphine',
-      email: 'Chaim_McDermott@dana.io',
-      image:'https://robohash.org/9?200x200'
-    },
-    {
-      id: 10,
-      name: 'Clementina DuBuque',
-      username: 'Moriah.Stanton',
-      email: 'Rey.Padberg@karina.biz',
-      image:'https://robohash.org/10?200x200'
-    }
-    ];
+const robots = [{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz',
+		image: 'https://robohash.org/1?200x200'
+	},
+	{
+		id: 2,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email: 'Shanna@melissa.tv',
+		image: 'https://robohash.org/2?200x200'
+	},
+	{
+		id: 3,
+		name: 'Clementine Bauch',
+		username: 'Samantha',
+		email: 'Nathan@yesenia.net',
+		image: 'https://robohash.org/3?200x200'
+	},
+	{
+		id: 4,
+		name: 'Patricia Lebsack',
+		username: 'Karianne',
+		email: 'Julianne.OConner@kory.org',
+		image: 'https://robohash.org/4?200x200'
+	},
+	{
+		id: 5,
+		name: 'Chelsey Dietrich',
+		username: 'Kamren',
+		email: 'Lucio_Hettinger@annie.ca',
+		image: 'https://robohash.org/5?200x200'
+	},
+	{
+		id: 6,
+		name: 'Mrs. Dennis Schulist',
+		username: 'Leopoldo_Corkery',
+		email: 'Karley_Dach@jasper.info',
+		image: 'https://robohash.org/6?200x200'
+	},
+	{
+		id: 7,
+		name: 'Kurtis Weissnat',
+		username: 'Elwyn.Skiles',
+		email: 'Telly.Hoeger@billy.biz',
+		image: 'https://robohash.org/7?200x200'
+	},
+	{
+		id: 8,
+		name: 'Nicholas Runolfsdottir V',
+		username: 'Maxime_Nienow',
+		email: 'Sherwood@rosamond.me',
+		image: 'https://robohash.org/8?200x200'
+	},
+	{
+		id: 9,
+		name: 'Glenna Reichert',
+		username: 'Delphine',
+		email: 'Chaim_McDermott@dana.io',
+		image: 'https://robohash.org/9?200x200'
+	},
+	{
+		id: 10,
+		name: 'Clementina DuBuque',
+		username: 'Moriah.Stanton',
+		email: 'Rey.Padberg@karina.biz',
+		image: 'https://robohash.org/10?200x200'
+	}
+];
 
 const main = document.querySelector("main");
 
 const createDiv = (robot) => {
-  let div = document.createElement("div");
-  div.setAttribute("id",robot.id);
+	let div = document.createElement("div");
+	div.setAttribute("id", robot.id);
 
-  let img = document.createElement("img");
-  img.setAttribute("src", robot.image);
-  div.appendChild(img);
+	let img = document.createElement("img");
+	img.setAttribute("src", robot.image);
+	div.appendChild(img);
 
-  let h2 = document.createElement("h2");
-  h2.textContent = robot.name;
-  div.appendChild(h2);
+	let h2 = document.createElement("h2");
+	h2.textContent = robot.name;
+	div.appendChild(h2);
 
-  let p = document.createElement("p");
-  p.textContent = robot.email;
-  div.appendChild(p);
+	let p = document.createElement("p");
+	p.textContent = robot.email;
+	div.appendChild(p);
 
-  main.appendChild(div);
+	main.appendChild(div);
 }
 
-for (robot of robots) {
- createDiv(robot);
+// let currentMatches = [];
+
+const createAll = () => {
+	for (robot of robots) {
+		createDiv(robot);
+		// currentMatches.push(robot.id);
+	}
 }
 
+createAll();
+
+let allDivs;
 const filter = () => {
-  let allDivs = document.querySelectorAll("div");
-  for (div of allDivs){
-    div.remove();
-  }
-  for (robot of robots) {
-    let array = robot.name.split(" ");
-    for (item of array) {
-      if (item.toLowerCase().search(input.value.toLowerCase()) === 0) {
-       createDiv(robot);
-       break;
-      }
-    }
-  }
-};
+	if (input == "") {
+		createAll();
+		return;
+	}
+	allDivs = document.querySelectorAll("div");
+	for (div of allDivs) {
+		div.remove();
+	}
+	let inputArray = input.value.trim().split(" "); //segments input if there are spaces
+	for (robot of robots) {
+		let matches = 0;	// track how many segments of input produce a match
+		for (segment of inputArray) {
+			let nameArray = robot.name.split(" "); 
+			for (partOfName of nameArray) { //any part of name (title, first, last) can produce a match
+				if (partOfName.toLowerCase().search(segment.toLowerCase()) === 0) {
+					matches++;
+					break; // so points don't accumulate if multiple parts of name happen to both match same segment
+				}
+			}
+		}
+		if (matches === inputArray.length) { // if all segments have a match
+		createDiv(robot);
+		}
+	}
+}
+
 
 let input = document.querySelector("input");
-input.addEventListener("keyup",filter);
-
-
-
+input.addEventListener("keyup", filter);
